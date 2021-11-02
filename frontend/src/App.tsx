7815f91e1ff97416a826a2789196931c3d548907
Fagner <fagner.ufpa@gmail.com>
2021-11-01 21:12:43 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ola Mundo....</h1>
+        <h1 className="text-primary">Ola Mundo....</h1>
       </header>
     </div>
   );
